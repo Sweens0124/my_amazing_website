c0f70a3b874d7c2146b89f4f6c001ee08954642c
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-06 10:31:58 -0700

    final website changes

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const h2 = document.createElement("h2")
 h2.textContent = "A little code about me!"
 
 document.querySelector("body").appendChild(h2)
+
+function show () {
+  document.getElementById('image')
+    .style.display = "block"
+
+  document.getElementById('btnID')
+    .style.display = "none"
+
+}
